d5b7737e01a7933dbe66b2ed718f2f6b1141f57b
Johnier Sanchez <jsanchezc16@uniminuto.edu.co>
2020-06-01 22:42:38 -0500

    FFin Parte Cuatro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { PreguntasfComponent } from './pages/preguntasf/preguntasf.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
