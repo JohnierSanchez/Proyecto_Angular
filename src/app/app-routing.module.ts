a7f8a0599d2d873381c3f0ef3e03522a184355f7
Johnier Sanchez <jsanchezc16@uniminuto.edu.co>
2020-06-01 21:38:53 -0500

    Fin Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PreguntasfComponent } from './pages/preguntasf/preguntasf.component';
 
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  {path: 'home', component: ProductosComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'item', component: ItemComponent },
+  {path: 'info', component: PreguntasfComponent},
+  {path: '**', pathMatch:'full', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(app_routes,{useHash: true})
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ProductosComponent } from './pages/productos/productos.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PreguntasfComponent } from './pages/preguntasf/preguntasf.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ProductosComponent
+    ProductosComponent,
+    AboutComponent,
+    ItemComponent,
+    PreguntasfComponent
   ],
   imports: [
     BrowserModule,
